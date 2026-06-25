7f6d9563246bcde04416878d756ac5e528d9be66
anistlh-star <anistlh@gmail.com>
2026-06-25 05:56:24 +0000

    your commit message

--- ecommerce/backend/controllers/authController.js
+++ ecommerce/backend/controllers/authController.js
@@ -9,7 +9,7 @@ import { welcomeTemplate } from "../templates/emailTemplate.js";
 /* Generate JWT */
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "1h",
   });
 };
 
